ec09ba8fba6c20599e26d927b8d769ca6585c4d4
Danil0101 <selyutin.d.g@gmail.com>
2020-06-20 09:12:39 +0300

    1. Модульные тесты. [#308831]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+// import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        Assert.assertEquals(2, Converter.rubleToEuro(140));
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        Assert.assertEquals(3, Converter.rubleToDollar(180));
+    }
+}
