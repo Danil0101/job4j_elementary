8c0a7081dba4dfd9e16ba70e9fedeac2a1e6223b
Danil0101 <selyutin.d.g@gmail.com>
2020-06-25 08:39:39 +0300

    6.0.2. Заполнение массива. [#308778]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] price = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "James Gosling";
+        names[1] = "Richard Stallman";
+        names[2] = "Bjarne Stroustrup";
+        names[3] = "Linus Torvalds";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
